27359854c22cf15fbb0810f63b37631b4dcdd237
Stephen Zeiler <stephennnlz@gmail.com>
2024-01-29 23:56:55 -0500

    test

--- DropperEquipment/src/main.cpp
+++ DropperEquipment/src/main.cpp
@@ -277,7 +277,7 @@ void setup()
 
 void loop()
 {
-  int s3aState = digitalRead(s3aPin);
+  int s3aState = AnalogRead(s3aPin);
   int homeButtonState = digitalRead(homeButtonPin);
   int startButtonState = digitalRead(startButtonPin);
   int stopButtonState = digitalRead(stopButtonPin);
