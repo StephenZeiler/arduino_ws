e761d16cc13ee7a9da3406030c8acd8567051dc1
Stephen Zeiler <stephennnlz@gmail.com>
2024-02-19 18:52:05 -0500

    test m2 m3 speed

--- DropperEquipment/src/main.cpp
+++ DropperEquipment/src/main.cpp
@@ -63,8 +63,8 @@ unsigned long previousM1Micros = 0;
 unsigned long previousM2Micros = 0;  
 long previousM3Micros = 0;  
 long m1Speed = 1000; // 1000 If change, must change slow start speed as well...
-long m2Speed = 200; //200
-long m3Speed = 250; //250
+long m2Speed = 150; //200
+long m3Speed = 200; //250
 double m1PulsePerRevMultiplier = 0.9; //.9 for 400, .45 for 800 on driver
 
 int encoderCurrentState;
@@ -311,11 +311,7 @@ void loop()
 
   if (productionRun)
   {
-
-    if(calculateDegrees(rotaryPosition)  < 180){
-        runMotorM1();
-      }
-    
+    runMotorM1();
     if(!slowStart){
       if(digitalRead(s2aPin)==HIGH){
         m2IsHome = true;
