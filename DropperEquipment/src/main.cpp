20bacb2bbee523bd392419bc2cf7d6e862049d99
Stephen Zeiler <stephennnlz@gmail.com>
2024-03-19 18:07:06 -0400

    m2 and m3 speed increased to 90/min...m1 remain

--- DropperEquipment/src/main.cpp
+++ DropperEquipment/src/main.cpp
@@ -74,8 +74,8 @@ unsigned long previousM2Micros = 0;
 unsigned long previousM3Micros = 0; 
 long previousHomeLEDMicros = 0;  
 long m1Speed = 1000; // 1000 is 70/min.... 750 = 90/min... If change, to change speed change the m1 speed in else of runMotorM1()...
-long m2Speed = 150; //150 is 70/min.... 95 = 90/min
-long m3Speed = 200; //200 is 70/min.... 130 = 90/min
+long m2Speed = 95; //150 is 70/min.... 95 = 90/min
+long m3Speed = 130; //200 is 70/min.... 130 = 90/min
 double m1PulsePerRevMultiplier = 0.9; //.9 for 400, .45 for 800 on driver
 bool ejectionFailed = false;
 bool ejectionDetected = false;
